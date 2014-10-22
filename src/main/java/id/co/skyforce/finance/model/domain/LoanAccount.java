3e50dadc0e03d6d7919be6c431f72751684d9d10
saputrasy <sandyyudhasaputra@gmail.com>
2014-10-22 13:29:33 +0700

    Mapping Hibernate.cfg

--- src/main/java/id/co/skyforce/finance/model/domain/LoanAccount.java
+++ src/main/java/id/co/skyforce/finance/model/domain/LoanAccount.java
@@ -17,79 +17,94 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "loan_account")
 public class LoanAccount {
-	
+
 	@Id
 	@Column(name = "account_no", length = 6, nullable = false)
 	private String accountNo;
-	
+
 	@Column(name = "plafond", precision = 15, scale = 2, nullable = false)
 	private BigDecimal plafond;
-	
+
 	@Column(name = "tenure", length = 11, nullable = false)
 	private Integer tenure;
-	
+
 	@Column(name = "interest_rate", precision = 5, scale = 2, nullable = false)
 	private BigDecimal interestRate;
-	
+
 	@Column(name = "interest_type", length = 1, nullable = false)
 	private Character interestType;
-	
+
 	@Column(name = "start_date", nullable = false)
 	private Date startDate;
-	
+
 	@OneToOne
 	@PrimaryKeyJoinColumn
 	private CIF cif;
-	
+
 	@OneToMany(cascade = CascadeType.ALL)
 	@JoinColumn(name = "account_no")
 	private Set<LoanAccountSchedule> loanAccountSchedules = new HashSet<>();
-	
+
 	public String getAccountNo() {
 		return accountNo;
 	}
+
 	public void setAccountNo(String accountNo) {
 		this.accountNo = accountNo;
 	}
+
 	public BigDecimal getPlafond() {
 		return plafond;
 	}
+
 	public void setPlafond(BigDecimal plafond) {
 		this.plafond = plafond;
 	}
+
 	public Integer getTenure() {
 		return tenure;
 	}
+
 	public void setTenure(Integer tenure) {
 		this.tenure = tenure;
 	}
+
 	public BigDecimal getInterestRate() {
 		return interestRate;
 	}
+
 	public void setInterestRate(BigDecimal interestRate) {
 		this.interestRate = interestRate;
 	}
+
 	public Character getInterestType() {
 		return interestType;
 	}
+
 	public void setInterestType(Character interestType) {
 		this.interestType = interestType;
 	}
+
 	public Date getStartDate() {
 		return startDate;
 	}
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+
 	public CIF getCif() {
 		return cif;
 	}
+
 	public void setCif(CIF cif) {
 		this.cif = cif;
 	}
+
 	public Set<LoanAccountSchedule> getLoanAccountSchedules() {
 		return loanAccountSchedules;
 	}
+
 	public void setLoanAccountSchedules(
 			Set<LoanAccountSchedule> loanAccountSchedules) {
 		this.loanAccountSchedules = loanAccountSchedules;
