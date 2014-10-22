c8e88d18ba86ed88ce3b14e244cf6f5d518387ba
saputrasy <sandyyudhasaputra@gmail.com>
2014-10-22 11:25:24 +0700

    id.co.skyforce.finance.model.domain LoanAccount

---
+++ src/main/java/id/co/skyforce/finance/model/domain/LoanAccount.java
@@ -0,0 +1,100 @@
+package id.co.skyforce.finance.model.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashSet;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "loan_account")
+public class LoanAccount {
+	
+	@Id
+	@Column(name = "account_no", nullable = false)
+	private String accountNo;
+	
+	@Column(name = "plafond", nullable = false)
+	private BigDecimal plafond;
+	
+	@Column(name = "tenure", nullable = false)
+	private Integer tenure;
+	
+	@Column(name = "interest_rate", nullable = false)
+	private BigDecimal interestRate;
+	
+	@Column(name = "interest_type", nullable = false)
+	private Character interestType;
+	
+	@Column(name = "start_date", nullable = false)
+	private Date startDate;
+	
+	@OneToOne
+	@PrimaryKeyJoinColumn
+	private CIF cif;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "account_no")
+	private set<LoanAccountSchedule> loanAccountSchedules = new HashSet<>();
+	
+	public String getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	public BigDecimal getPlafond() {
+		return plafond;
+	}
+	public void setPlafond(BigDecimal plafond) {
+		this.plafond = plafond;
+	}
+	public Integer getTenure() {
+		return tenure;
+	}
+	public void setTenure(Integer tenure) {
+		this.tenure = tenure;
+	}
+	public BigDecimal getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(BigDecimal interestRate) {
+		this.interestRate = interestRate;
+	}
+	public Character getInterestType() {
+		return interestType;
+	}
+	public void setInterestType(Character interestType) {
+		this.interestType = interestType;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public CIF getCif() {
+		return cif;
+	}
+	public void setCif(CIF cif) {
+		this.cif = cif;
+	}
+	public set<LoanAccountSchedule> getLoanAccountSchedules() {
+		return loanAccountSchedules;
+	}
+	public void setLoanAccountSchedules(
+			set<LoanAccountSchedule> loanAccountSchedules) {
+		this.loanAccountSchedules = loanAccountSchedules;
+	}
+	
+}
