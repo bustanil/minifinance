9a1c012b39069abd28e7eb917ac1fde06be19916
Randytia <Randytia.akbar@gmail.com>
2014-10-24 14:37:19 +0700

    save

--- src/main/java/id/co/skyforce/finance/service/CIFService.java
+++ src/main/java/id/co/skyforce/finance/service/CIFService.java
@@ -12,6 +12,25 @@ import id.co.skyforce.finance.util.HibernateUtil;
 
 public class CIFService {
 
+	private List<CIF> cifs;
+
+	public List<CIF> getCifs() {
+		return cifs;
+	}
+
+	public void setCifs(List<CIF> cifs) {
+		this.cifs = cifs;
+	}
+
+	public void listCif() {
+
+		Session session = HibernateUtil.openSession();
+		Transaction transaction = session.beginTransaction();
+
+		cifs = session.createQuery("from CIF").list();
+		
+	}
+
 	public String addUpdateCif(CIF cif) {
 		Transaction transaction = null;
 		Session session = HibernateUtil.openSession();
@@ -71,26 +90,26 @@ public class CIFService {
 		return cifs;
 	}
 
-	public List<CIF> searchByNameBirthDayMother(String search) {
-		Transaction transaction = null;
-		Session session = HibernateUtil.openSession();
-		try {
-			transaction = session.beginTransaction();
-			Query query = session
-					.createQuery("FROM CIF where firstName LIKE :fn OR lastName LIKE :ln OR birthDate LIKE :bd OR motherMaidenName LIKE :md");
-			query.setString("fn", "%" + search + "%");
-			query.setString("ln", "%" + search + "%");
-			query.setString("bd", "%" + search + "%");
-			query.setString("md", "%" + search + "%");
-			List<CIF> listCifs = query.list();
-			if (listCifs.size() > 0) {
-				return listCifs;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			transaction.rollback();
-		}
-		session.close();
-		return null;
-	}
+	// public List<CIF> searchByNameBirthDayMother(String search) {
+	// Transaction transaction = null;
+	// Session session = HibernateUtil.openSession();
+	// try {
+	// transaction = session.beginTransaction();
+	// Query query = session
+	// .createQuery("FROM CIF where firstName LIKE :fn OR lastName LIKE :ln OR birthDate LIKE :bd OR motherMaidenName LIKE :md");
+	// query.setString("fn", "%" + search + "%");
+	// query.setString("ln", "%" + search + "%");
+	// query.setString("bd", "%" + search + "%");
+	// query.setString("md", "%" + search + "%");
+	// List<CIF> listCifs = query.list();
+	// if (listCifs.size() > 0) {
+	// return listCifs;
+	// }
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// transaction.rollback();
+	// }
+	// session.close();
+	// return null;
+	// }
 }
