006d3ea554132d17facc86817681ec93318b82b7
saputrasy <sandyyudhasaputra@gmail.com>
2014-10-27 15:47:54 +0700

    Simulate LoanAccount ajax yess ! :D

--- src/main/java/id/co/skyforce/finance/controller/LoanAccountController.java
+++ src/main/java/id/co/skyforce/finance/controller/LoanAccountController.java
@@ -12,8 +12,10 @@ import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 @ManagedBean
+@ViewScoped
 public class LoanAccountController {
 
 	private String accountNo;
