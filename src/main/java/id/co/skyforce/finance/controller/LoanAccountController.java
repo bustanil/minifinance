e1f9da68b4f9855859f1b864c52324d1b927911a
saputrasy <sandyyudhasaputra@gmail.com>
2014-10-27 10:43:38 +0700

    Simulate LoanAccountSchedule

--- src/main/java/id/co/skyforce/finance/controller/LoanAccountController.java
+++ src/main/java/id/co/skyforce/finance/controller/LoanAccountController.java
@@ -14,7 +14,7 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class LoanAccountController {
-	
+
 	private String accountNo;
 	private BigDecimal plafond;
 	private Integer tenure;
@@ -22,60 +22,81 @@ public class LoanAccountController {
 	private Character interestType;
 	private Date startDate;
 	private String cifNo;
-	private List<LoanAccountSchedule> loanAccountSchedules;
-	
+	private List<LoanAccountSchedule> loanAccounts;
+
 	public void simulate() throws Exception {
-		LoanAccount loanAccounts = new LoanAccount();
-		loanAccounts.setAccountNo(accountNo);
-		loanAccounts.setPlafond(plafond);
-		loanAccounts.setTenure(tenure);
-		loanAccounts.setInterestType(interestType);
-		loanAccounts.setStartDate(startDate);
-		loanAccounts.setInterestRate(interestRate);
-		LoanAccountScheduleService.generateSchedule(loanAccounts);
-		LoanAccountService loanAccountService = new LoanAccountService();
-		loanAccountService.addAcount(loanAccounts);
-	}	
-		
+		LoanAccount loanAccount = new LoanAccount();
+		loanAccount.setAccountNo(accountNo);
+		loanAccount.setPlafond(plafond);
+		loanAccount.setTenure(tenure);
+		loanAccount.setInterestType(interestType);
+		loanAccount.setStartDate(new Date());
+		loanAccount.setInterestRate(interestRate);
+		LoanAccountScheduleService.generateSchedule(loanAccount);
+
+		this.loanAccounts = loanAccount.getLoanAccountSchedules();
+	}
+
+	public List<LoanAccountSchedule> getLoanAccounts() {
+		return loanAccounts;
+	}
+
+	public void setLoanAccounts(List<LoanAccountSchedule> loanAccounts) {
+		this.loanAccounts = loanAccounts;
+	}
+
 	public String getAccountNo() {
 		return accountNo;
 	}
+
 	public void setAccountNo(String accountNo) {
 		this.accountNo = accountNo;
 	}
+
 	public BigDecimal getPlafond() {
 		return plafond;
 	}
+
 	public void setPlafond(BigDecimal plafond) {
 		this.plafond = plafond;
 	}
+
 	public Integer getTenure() {
 		return tenure;
 	}
+
 	public void setTenure(Integer tenure) {
 		this.tenure = tenure;
 	}
+
 	public BigDecimal getInterestRate() {
 		return interestRate;
 	}
+
 	public void setInterestRate(BigDecimal interestRate) {
 		this.interestRate = interestRate;
 	}
+
 	public Character getInterestType() {
 		return interestType;
 	}
+
 	public void setInterestType(Character interestType) {
 		this.interestType = interestType;
 	}
+
 	public Date getStartDate() {
 		return startDate;
 	}
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+
 	public String getCifNo() {
 		return cifNo;
 	}
+
 	public void setCifNo(String cifNo) {
 		this.cifNo = cifNo;
 	}
