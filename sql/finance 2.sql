2aea8a42720e9435d4e76ab9fed79ecf34b55a03
randytia <randytia.akbar@gmail.com>
2014-10-29 00:58:39 +0700

    Finance.sql (2)

---
+++ sql/finance 2.sql
@@ -0,0 +1,93 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.16 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             8.3.0.4694
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping database structure for finance
+CREATE DATABASE IF NOT EXISTS `finance` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `finance`;
+
+
+-- Dumping structure for table finance.cif
+CREATE TABLE IF NOT EXISTS `cif` (
+  `cif_no` varchar(8) NOT NULL DEFAULT '',
+  `password` varchar(10) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `mother_maiden_name` varchar(50) DEFAULT NULL,
+  `id_number` varchar(50) DEFAULT NULL,
+  `id_type` char(1) DEFAULT NULL,
+  PRIMARY KEY (`cif_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table finance.loan_account
+CREATE TABLE IF NOT EXISTS `loan_account` (
+  `account_no` varchar(6) NOT NULL DEFAULT '',
+  `plafond` decimal(15,2) DEFAULT NULL,
+  `tenure` int(11) DEFAULT NULL,
+  `interest_rate` decimal(5,2) DEFAULT NULL,
+  `interest_type` char(1) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `cif_no` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`account_no`),
+  KEY `FK_loan_account_cif` (`cif_no`),
+  CONSTRAINT `FK_loan_account_cif` FOREIGN KEY (`cif_no`) REFERENCES `cif` (`cif_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table finance.loan_account_schedule
+CREATE TABLE IF NOT EXISTS `loan_account_schedule` (
+  `account_no` varchar(6) NOT NULL DEFAULT '',
+  `period` int(11) NOT NULL DEFAULT '0',
+  `due_date` date DEFAULT NULL,
+  `principal` decimal(15,2) DEFAULT NULL,
+  `interest` decimal(15,2) DEFAULT NULL,
+  `installment` decimal(15,2) DEFAULT NULL,
+  `outstanding` decimal(15,2) DEFAULT NULL,
+  `paid_status` char(1) DEFAULT NULL,
+  PRIMARY KEY (`account_no`,`period`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table finance.payment
+CREATE TABLE IF NOT EXISTS `payment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `transaction_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `amount` decimal(15,2) DEFAULT NULL,
+  `account_no` varchar(8) DEFAULT NULL,
+  `payment_status` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table finance.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(8) DEFAULT NULL,
+  `last_login` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
